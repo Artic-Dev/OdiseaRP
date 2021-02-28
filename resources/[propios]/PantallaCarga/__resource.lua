f6056ff023da036c8eafa7f422f7b54e08656e40
Jose Garcia Garcia <47431411+Blazester@users.noreply.github.com>
2021-02-28 19:44:11 +0100

    Loading

--- resources/[propios]/PantallaCarga/__resource.lua
+++ resources/[propios]/PantallaCarga/__resource.lua
@@ -3,8 +3,9 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 files{
     'index.html',
     'css/style.css',
-    'js/main.js',
-    'media/video.mp4'
+    'Logo.png',
+    'css/normalize.css',
+    'Logo_de_Odisea_RP.png'
 }
 
 loadscreen 'index.html'
